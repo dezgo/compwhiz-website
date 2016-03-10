a7220613606f61230a17e39830bbce5828883641
Derek <github@derekgillett.com>
2016-03-10 12:38:22 +1100

    new website design by esposita

--- README.md
+++
@@ -1,2 +0,0 @@
-compwhiz-website
-================
--- setup/insert_test_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `tblClients`(`intID`, `varFirstname`, `varLastname`, `varEmail`, `varPassword`, `intActive`, `varActivationKey`) 
-VALUES 
-(0,'Derek','Gillett','derekgg@gmail.com','1234',0,'');
