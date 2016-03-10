7b18bd5fa36ac3424d1bd87533c75dfd2f81f086
Derek <github@derekgillett.com>
2016-03-10 13:37:19 +1100

    first take on business-focused site

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -2,74 +2,68 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-6">
-        Hi, I'm Derek Gillett and I run Computer Whiz - Canberra. This is my own small business which I've run
-        from my home since 2009. I previously ran a similar business in Brisbane before moving to Canberra.<br />
-        <br />
-        I'm passionate about all things I.T. My aim is to provide expert technical advice and assistance
-        in a friendly, easy-to-understand manner to the local community.<br />
-        <br />
-        I currently run this business part-time when not at my full-time job, so I'm usually available weeknights
-        and weekends.<br />
-        <br />
+        <div class="col-md-8">
+        Hi, I'm Derek Gillett and I run Computer Whiz - Canberra. My goal is to provide a high-quality,
+        timely, professional service to small businesses in Canberra and Queanbeyan.<Br>
+        <br>
+        High-quality, because I have over 20 years experience in the I.T. field. I've run I.T. support
+        businesses both locally and interstate as well as worked as an independent consultant, and with
+        a large Canberra firm.<br>
+        <br>
+        And timely because prompt service is important. You have a business to run, and when things go
+        amiss, you need to quickly get back up and running.<br>
+        <br>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <img src="{{asset('/images/selfie_bw.jpg')}}">
         </div>
     </div>
 
-    <h2>Pricing</h2>
-    <p>
-        Computer Whiz pricing is super simple - $120/hr with a minimum 1 hour, then charged in 15min increments after that.
-        I'm also happy to provide remote support, in which case there's no minimum.
-    </p>
-    <p>
-        If you decide to book online, you can also pay with a credit card. Otherwise, I'll happily accept cash,
-        or bank deposit on the day.
-    </p>
+    <div class="row">
+        <div class="col-md-4">
+            <img src="{{asset('/images/phone-1160874_640-small.png')}}">
+        </div>
+        <div class="col-md-8">
+        <h2>Services</h2>
+        <p>
+            Aging technology, but unsure what to do? A business of any size needs lots of I.T. support. Things like:
+        </p>
+            <ul>
+                <li>Up-to-date antivirus software</li>
+                <li>Advice / assistance when purchasing new hardware</li>
+                <li>A robust disaster recovery plan</li>
+                <li>Staff training</li>
+                <li>Malware / virus removal</li>
+                <li>Regular PC/network health checks</li>
+                <li>A high quality backup system</li>
+                <li>Simple remote support for quick resolution to simple issues</li>
+            </ul>
+        <p>
+            Plus, you may have a lot of different types of hardware, such as:
+        </p>
+            <ul>
+                <li>Desktop / Laptop computers (PC and Mac)</li>
+                <li>Mobiles / Tablets (iOS and Android)</li>
+                <li>Servers / network infrastructure</li>
+            </ul>
+        </div>
+    </div>
 
-    <h2>Services</h2>
-    <p>
-        You name it, if it's technical, I can probably help! Here's a sample of some of the areas my customers
-        typically need help with
-    </p>
-        <ul>
-            <li>Setup of new systems</li>
-            <li>Data back up</li>
-            <li>New computer sales</li>
-            <li>Virus removal</li>
-            <li>Internet security</li>
-            <li>Training</li>
-            <li>PC health checks</li>
-            <li>Printer setup / troubleshooting</li>
-        </ul>
-    <p>
-        I support all sorts of equipment
-    </p>
-        <ul>
-            <li>Desktop / Laptop computers (PC and Mac)</li>
-            <li>Smart TVs</li>
-            <li>Mobiles / Tablets (iOS and Android)</li>
-        </ul>
-
-    <h2>Contact</h2>
-    <p>
-        This is a part-time business for me, which means I can't take calls during business hours. When you contact
-        me using any method below, I will be sent an email and will respond to you ASAP.
-    </p>
-    <p>
-        For a quick response, try out the bookings website. Once you book, both you and I will receive an email
-        confirmation. If you change your mind or need to reschedule, just contact me and we can arrange it.
-        You can even pre-pay by credit card when you book.
-    </p>
-    <p>
-        Email: <a href="mailto:web@computerwhiz.com.au">web@computerwhiz.com.au</a><br>
-    </p>
-    <p>
-        Phone: 02 6112 8025 (direct to voicemail).
-    </p>
-    <p>
-        Book online: <a target="_blank" href="http://computerwhiz.simplybook.me/">http://computerwhiz.simplybook.me</a>
-    </p>
-
+    <div class="row">
+        <div class="col-md-8">
+        <h2>Contact</h2>
+        <p>
+            Email: <a href="mailto:web@computerwhiz.com.au">web@computerwhiz.com.au</a><br>
+        </p>
+        <p>
+            Phone: 02 6112 8025 (direct to voicemail).
+        </p>
+        <p>
+            Book online: <a target="_blank" href="http://computerwhiz.simplybook.me/">http://computerwhiz.simplybook.me</a>
+        </p>
+        </div>
+        <div class="col-md-4">
+            <img width="320" height="320" src="{{asset('/images/call-center-1027585_640.jpg')}}">
+        </div>
+    </div>
 @stop
