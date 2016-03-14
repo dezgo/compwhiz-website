1b7d9db6e4b0a9150b5c3cb5c7115617a9d13752
Derek Gillett <github@derekgillett.com>
2016-03-14 19:50:28 +0000

    update contact form

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -53,13 +53,91 @@
         <div class="col-md-8">
         <h2>Contact</h2>
         <p>
+            Location: Red Hill, ACT
+        </p>
+        <p>
             Email: <a href="mailto:web@computerwhiz.com.au">web@computerwhiz.com.au</a><br>
         </p>
         <p>
-            Phone: 02 6112 8025 (direct to voicemail).
+            Office: 02 6112 8025
+        </p>
+        <p>
+            Mobile: 0474 029 265
         </p>
         <p>
             Book online: <a target="_blank" href="http://computerwhiz.simplybook.me/">http://computerwhiz.simplybook.me</a>
+        </p>
+        <p>
+            Request a callback:
+
+            @if ($message == '')
+            <form class="form-horizontal" action="/callback" method="post">
+                {{ csrf_field() }}
+              <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                <label for="name" class="col-sm-2 control-label">Name</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="{{ old('name') }}">
+                    @if ($errors->has('name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                    @endif
+                </div>
+              </div>
+              <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
+                <label for="phone_number" class="col-sm-2 control-label">Number</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" name="phone_number" id="phone_number" placeholder="Number" value="{{ old('phone_number') }}">
+                    @if ($errors->has('phone_number'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('phone_number') }}</strong>
+                        </span>
+                    @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                <label for="best_time" class="col-sm-2 control-label">Best time</label>
+                <div class="col-sm-8">
+                    <select class="form-control" name="best_time" id="best_time">
+                        <option></option>
+                        <option{{ old('best_time') == '8:00 AM' ? ' selected' : '' }} value="8:00 AM">8:00 AM</option>
+                        <option{{ old('best_time') == '9:00 AM' ? ' selected' : '' }} value="9:00 AM">9:00 AM</option>
+                        <option{{ old('best_time') == '10:00 AM' ? ' selected' : '' }} value="10:00 AM">10:00 AM</option>
+                        <option{{ old('best_time') == '11:00 AM' ? ' selected' : '' }} value="11:00 AM">11:00 AM</option>
+                        <option{{ old('best_time') == '12:00 PM' ? ' selected' : '' }} value="12:00 PM">12:00 PM</option>
+                        <option{{ old('best_time') == '1:00 PM' ? ' selected' : '' }} value="1:00 PM">1:00 PM</option>
+                        <option{{ old('best_time') == '2:00 PM' ? ' selected' : '' }} value="2:00 PM">2:00 PM</option>
+                        <option{{ old('best_time') == '3:00 PM' ? ' selected' : '' }} value="3:00 PM">3:00 PM</option>
+                        <option{{ old('best_time') == '4:00 PM' ? ' selected' : '' }} value="4:00 PM">4:00 PM</option>
+                        <option{{ old('best_time') == '5:00 PM' ? ' selected' : '' }} value="5:00 PM">5:00 PM</option>
+                        <option{{ old('best_time') == '6:00 PM' ? ' selected' : '' }} value="6:00 PM">6:00 PM</option>
+                        <option{{ old('best_time') == '7:00 PM' ? ' selected' : '' }} value="7:00 PM">7:00 PM</option>
+                        <option{{ old('best_time') == '8:00 PM' ? ' selected' : '' }} value="8:00 PM">8:00 PM</option>
+                    </select>
+                </div>
+              </div>
+              <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
+                <label for="message" class="col-sm-2 control-label">Message</label>
+                <div class="col-sm-8">
+                    <textarea class="form-control" name="message" id="message" cols='30' rows='3'>{{ old('message') }}</textarea>
+                    @if ($errors->has('message'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('message') }}</strong>
+                        </span>
+                    @endif
+                </div>
+              </div>
+              <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-8">
+                      <button type="submit" class="btn btn-default">Request</button>
+                  </div>
+              </div>
+            </form>
+            @else
+            <div class="alert alert-success" role="alert">
+              {{ $message }}
+            </div>
+            @endif
         </p>
         </div>
         <div class="col-md-4">
---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,6 @@
+Here's a message someone left on the website:<br />
+<br />
+Name: {{ $request->name}}<br />
+Number: {{ $request->phone_number }}<br />
+Best Time: {{ $request->best_time }}<br />
+Message: {{$request->message }}<br />
