fc5767139eb271628c25cc1cf410bfa60aba0679
derek <github@derekgillett.com>
2016-03-31 13:11:40 +1100

    add support info

---
+++ resources/views/content/support-info.blade.php
@@ -0,0 +1,75 @@
+@extends('master')
+
+@section('content')
+<h1>Business Support Information Sheet</h1>
+As at 31 March 2016<br />
+<Br />
+<h2>Services</h2>
+<h3>Automated monitoring of key infrastructure</h3>
+<ul>
+    <li>Website uptime</li>
+    <li>Website changes</li>
+    <li>Desktops / Servers (disk health, antivirus software, patches, and more)</li>
+</ul>
+<br />
+<h3>Backup configuration</h3>
+<ul>
+    <li>
+        Set-and-forget backup setup so critical data is backed up in real time
+    </li>
+    <li>
+        Regular email reports showing backup status
+    </li>
+</ul>
+<br />
+<h3>Virus / Malware protection</h3>
+<ul>
+    <li>
+        High quality antivirus software ensures PC stay protected whilst online
+    </li>
+    <li>
+        Best-of-class malware detection and removal software used to block all types of malicious software
+    </li>
+</ul>
+<br />
+<h3>Website design / hosting</h3>
+<ul>
+    <li>
+        Design done by experienced Graphic Designer
+    </li>
+    <li>
+        Hosted using secure New York-based servers with weekly snapshot backups
+    </li>
+    <li>
+        Add interactive content such as Contact and Booking forms
+    </li>
+</ul>
+<br />
+<h3>Quick response times to critical issues</h3>
+<ul>
+    <li>
+        As a Canberra-based business we can provide excellent response times for onsite as well as remote support
+    </li>
+    <li>
+        Issues dealt with in order of severity to ensure critical issues are dealt with immediately
+    </li>
+</ul>
+
+<br />
+<h2>Flexible Engagement Model</h2>
+Based centrally in Red Hill, we can provide timely onsite support for issues where remote support is unavailable or impractical. Minimum charge 1 hour.<br />
+<br />
+Remote support charged in 15 minute increments. Option to allow unattended access, or ad-hoc access via a small downloaded file where permission is granted on the device being controlled.
+<Br />
+<h2>Fee Schedule*</h2>
+Support agreement: 1 hour / month paid yearly in advance at ad-hoc rate below. Includes up to 1 hour of remote support.<br />
+<Br />
+Ad-hoc hourly rate: ${{ $rates[0] * 1.1 }} / hour<br />
+Block of 10 hours: ${{ $rates[1] * 1.1 * 10 }} (${{ $rates[1] * 1.1 }} / hour)<br />
+Block of 20 hours: ${{ $rates[2] * 1.1 * 20 }} (${{ $rates[2] * 1.1 }} / hour)<br />
+<br />
+Note, block hours must be purchased in advance. If no hours credit remain, ad-hoc rate will be charged.<br />
+<br />
+* all rates inclusive of GST where applicable
+
+@stop
